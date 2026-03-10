02a1368cd3869ef3eabcfb9da789f149d491e46a
Peter Swinkels <peter_swinkels@hotmail.com>
2026-03-10 17:30:03 +0100

    Add files via upload

---
+++ 8086 Emulator Trace Cleanup/Core.vb
@@ -0,0 +1,63 @@
+﻿'This module's imports and settings.
+Option Compare Binary
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Imports System
+Imports System.Environment
+Imports System.IO
+Imports System.Linq
+Imports System.Text
+
+'This module contains this program's core procedures.
+Public Module CoreModule
+
+   'This program is executed when this program is started.
+   Public Sub Main()
+      Try
+         Dim Block As New StringBuilder
+         Dim Count As Integer = 0
+         Dim InputFile As String = Nothing
+         Dim OutputFile As String = $"{InputFile}.tmp"
+         Dim OutputText As New StringBuilder
+         Dim PreviousBlock As String = Nothing
+
+         If GetCommandLineArgs().Count < 2 Then
+            Console.WriteLine("No input file specified.")
+         Else
+            InputFile = GetCommandLineArgs().Last()
+
+            For Each Line As String In File.ReadAllLines(InputFile)
+
+               If Line = Nothing Then
+                  If Block.ToString() = PreviousBlock Then
+                     Count += 1
+                  Else
+                     OutputText.Append($"{Block}{NewLine}")
+                  End If
+                  PreviousBlock = Block.ToString()
+                  Block.Clear()
+               Else
+                  Block.Append($"{Line}{NewLine}")
+               End If
+            Next Line
+
+            If Block.ToString() IsNot Nothing Then
+               OutputText.Append(Block)
+            End If
+
+            File.WriteAllText(OutputFile, OutputText.ToString())
+            File.Delete(InputFile)
+            File.Move(OutputFile, InputFile)
+
+            Console.WriteLine($"{Count} duplicate trace events removed.")
+         End If
+      Catch [Exception] As Exception
+         Console.WriteLine($"ERROR: {[Exception]}")
+      End Try
+
+      Console.ReadLine()
+   End Sub
+
+End Module
---
+++ 8086 Emulator Trace Cleanup/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("8086 Emulator Trace Cleanup")>
+<Assembly: AssemblyDescription("This program cleans up trace logs from 8086 Emulator by removing duplicate entries.")>
+<Assembly: AssemblyCompany("Peter Swinkels")>
+<Assembly: AssemblyProduct("8086 Emulator Trace Cleanup")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a31e324f-4a5f-4454-9292-9f55d748e068")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
